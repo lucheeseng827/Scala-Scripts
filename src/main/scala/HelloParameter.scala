f9d35af50165f9a6659d16520944ff8e21381542
Nick Lu Chee Seng <lu.cheeseng827@gmail.com>
2019-04-10 16:11:45 +0700

    Initial commit

---
+++ src/main/scala/HelloParameter.scala
@@ -0,0 +1,6 @@
+object HelloParameter {
+  def main(args: Array[String]): Unit = {
+
+  }
+
+}
---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,7 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("strings!!!")
+  }
+}
+
+//set a hello world  printout
